17c418ef3437fc0a2322f6b7ef5399659ad7087d
adribasterra <adrianabasterraa@gmail.com>
2021-05-29 17:32:17 +0200

    Feat: basic behaviour

--- Scripts/AgentController.cs
+++ Scripts/AgentController.cs
@@ -7,18 +7,46 @@ using Unity.MLAgents.Actuators;
 
 public class AgentController : Agent
 {
+    [SerializeField] private GameObject target;
+    [Range(30f, 50f)] public float speed;
+
+    public override void OnEpisodeBegin()
+    {
+        this.transform.position = new Vector3(0f, this.transform.position.y, 0f);
+    }
+
     public override void CollectObservations(VectorSensor sensor)
     {
-        base.CollectObservations(sensor);
+        sensor.AddObservation(this.transform.position);
+        sensor.AddObservation(target.transform.position);
     }
 
     public override void OnActionReceived(ActionBuffers actions)
     {
-        base.OnActionReceived(actions);
+        float moveX = actions.ContinuousActions[0];
+        float moveZ = actions.ContinuousActions[1];
+        
+        this.transform.position += new Vector3(moveX, 0f, moveZ) * speed * Time.deltaTime;
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+        continuousActions[0] = Input.GetAxisRaw("Horizontal");
+        continuousActions[1] = Input.GetAxisRaw("Vertical");
     }
 
     public void OnTriggerEnter(Collider other)
     {
-        SetReward(1f);
+        if(other.gameObject.tag == "wall")
+        {
+            SetReward(-1f);
+            EndEpisode();
+        }
+        if (other.gameObject.tag == "target")
+        {
+            SetReward(+1f);
+            EndEpisode();
+        }
     }
 }
