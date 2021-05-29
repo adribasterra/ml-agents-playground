fd80470e65bf2e4c8cae20a25576ab2d0243b897
adribasterra <adrianabasterraa@gmail.com>
2021-05-29 14:11:45 +0200

    Init

---
+++ Scripts/AgentController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
+
+public class AgentController : Agent
+{
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        base.CollectObservations(sensor);
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        base.OnActionReceived(actions);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        SetReward(1f);
+    }
+}
